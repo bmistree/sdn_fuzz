ce27236a12b80a0aea00e75c601d7608d5b0ea25
bmistree <bmistree@gmail.com>
2014-11-18 13:35:38 -0800

    Added code for creating a tcp sdn socket.

---
+++ sdn_fuzz/sdnsocket/tcp_sdnsocket.py
@@ -0,0 +1,46 @@
+import threading
+
+class TCPSDNSocket(object):
+
+    def __init__(self,socket):
+        '''
+        @param {BSD socket} socket --- Should already have been
+        connected or accepted.
+        '''
+        self.socket = socket
+        self.read_lock = threading.RLock()
+        self.write_lock = threading.RLock()
+    
+    def read(self,num_bytes_to_read):
+        '''
+        Threadsafe.
+        
+        @returns {bytearray} --- Returns at most num_bytes bytes.
+        '''
+        assert(False,'Only allow blocking reads on tcp.')
+
+    def blocking_read(self,num_bytes_to_read):
+        '''
+        Threadsafe.  Doesn't return until can return some bytes.
+
+        @returns {bytearray} --- Returns at most num_bytes bytes.
+        '''
+        with self.read_lock:
+            return self.socket.recv(num_bytes_to_read)
+
+    def write_into_read(self,bytes_to_write):
+        '''
+        Threadsafe.  Write into read buffer.
+        
+        @param {bytearray} bytes_to_write
+        '''
+        assert(False,'Disallowing write_into_read for tcp sockets')
+
+    def write(self, bytes_to_write):
+        '''
+        Threadsafe.  Write into other endpoint's buffer.
+
+        @param {bytearray} bytes_to_write
+        '''
+        with self.write_lock:
+            self.socket.sendall(bytes_to_write)
