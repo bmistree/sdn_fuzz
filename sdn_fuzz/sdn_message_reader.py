641ffe88655f4a8d7c17a05a0a4c8c63d3f28833
bmistree <bmistree@gmail.com>
2014-12-23 18:25:30 -0800

    Bypassing features reply because it seems to be incorrectly parsed in ryu.

--- sdn_fuzz/sdn_message_reader.py
+++ sdn_fuzz/sdn_message_reader.py
@@ -39,9 +39,18 @@ class SDNMessageReader(object):
         while len(msg_buffer) != msg_len:
             diff = msg_len - len(msg_buffer)
             msg_buffer.extend(reader_method(diff))
-
+        
         msg = ofproto_parser.msg(
             _OF_1_0_DATAPATH, version, msg_type, msg_len,
             xid, str(msg_buffer))
 
+        if msg is None:
+            import pdb
+            pdb.set_trace()
+            msg = ofproto_parser.msg(
+                _OF_1_0_DATAPATH, version, msg_type, msg_len,
+                xid, str(msg_buffer))
+            
+            print '\nThis is msg_type: ' + str(msg_type) + '\n'
+        
         return msg
